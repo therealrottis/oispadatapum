72666b9d6739ccbb8a2e91b597b9bd99e0f5b9c6
Onni Ruohola <onni.ruohola29@gmail.com>
2024-09-24 11:34:07 +0300

    even more cookies

--- index.js
+++ index.js
@@ -356,11 +356,11 @@ function gameOver(){
   console.log("jai");
 }
 
-function setCookie(name, value, days) {
+function setCookie(name, value, days, path = "/") {
   const d = new Date();
   d.setTime(d.getTime() + (days*24*60*60*1000));
   const expires = "expires=" + d.toUTCString();
-  document.cookie = name + "=" + value + ";" + expires + ";path=/";
+  document.cookie = name + "=" + value + ";" + expires + ";path=" + path;
 }
 
 async function setEncryptedCookie(score) {
